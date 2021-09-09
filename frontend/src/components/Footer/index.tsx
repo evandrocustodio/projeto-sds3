2f923ff9f9019ebd9331824c932d4138ededa6e2
Evandro Custodio <evandrocustodio@gmail.com>
2021-09-08 22:14:36 -0300

    Atualizando link do Instagram

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/evandrocustodio" target="_blank" rel="noreferrer">Evandro Custodio</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@evandrocgoncalves</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/evandrocgoncalves" target="_blank" rel="noreferrer">@evandrocgoncalves</a></small></p>
             </div>
         </footer>);
 }
