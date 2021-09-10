4e6a311247244ff4f689146cf9ffa43811ada96b
Evandro Custodio <evandrocustodio@gmail.com>
2021-09-10 14:06:47 -0300

    Adicionando BarChart Integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/request";
 
 const BarChart = () => {
+
+    type serialData = {
+        name:string;
+        data: string[];
+     }
+
+    type ChartData = {
+        labels: { categories: string[]},
+        series: serialData[],
+    }
+    const [chartData,setChartData] = useState<ChartData>({labels: { categories: []}, series: [{ name : '', data: []}]});
+
+    
     const options = {
         plotOptions: {
             bar: {
@@ -9,21 +27,20 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-sellers`)
+        .then( response => { 
+             const data = response.data as SaleSuccess[];
+             const myLabels = data.map(x => x.sellerName);
+             const mySeries = data.map(x =>  round(100 * x.deals / x.visited, 3).toFixed(2));
 
+            setChartData({labels: {categories: myLabels} , series: [{name: '% Sucess', data: mySeries}]});
+        });
+    },[]);    
+    
     return (
         <div id="chart">
-            <Chart options={{...options, xaxis: mockData.labels}} series={mockData.series} type="bar" height={240} />
+            <Chart options={{...options, xaxis: chartData.labels}} series={chartData.series} type="bar" height={240} />
         </div>)
 }
 
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -3,6 +3,7 @@ import Chart from "react-apexcharts";
 import axios from 'axios';
 import { BASE_URL } from 'utils/request';
 import { SaleSum } from 'types/sale';
+import { round } from 'utils/format';
 
 type ChartData = {
     labels: string[],
